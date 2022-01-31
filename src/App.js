7f39f909deceb3cfac71f14d888feb660cab442f
PalomaWolfango <paloma.wolfango@gmail.com>
2022-01-31 14:58:35 +0000

    first_dev

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React1
+          Learn React Teste
         </a>
       </header>
     </div>
