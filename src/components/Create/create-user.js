80dcbd3dc4c9509163d63e7407469598c9c19179
PalomaWolfango <paloma.wolfango@gmail.com>
2022-02-06 12:29:06 +0000

    inserir_user

--- src/components/Create/create-user.js
+++ src/components/Create/create-user.js
@@ -1,5 +1,6 @@
 import React from "react";
 import MenuAdmin from "../Menu/menu-admin";
+import axios from "axios";
 
 class CreateUser extends React.Component {
     render(){
@@ -8,26 +9,26 @@ class CreateUser extends React.Component {
             <MenuAdmin />
             <h1 style={{color: 'grey', margin: '15px', marginTop: '100px', textAlign: 'center'}}>Create User</h1>
             <div className="container" style={{ border: '3px solid #00867d', padding: '50px 35px', marginTop: '90px', maxWidth: '500px' }}>
-                <form>
-                    <div class="mb-3">
-                        <input type="text" class="form-control"  placeholder="Name"/>
+                <form onSubmit={(e) => createUser(e)}>
+                    <div className="mb-3">
+                        <input type="text" id="validationDefaultName" className="form-control"  placeholder="Name"/>
                     </div>
-                    <div class="mb-3">
-                        <input type="text" class="form-control" placeholder="Contact" />
+                    <div className="mb-3">
+                        <input type="text" id="validationDefaultContact" className="form-control" placeholder="Email" />
                     </div>
-                    <div class="mb-3">
-                        <input type="text" class="form-control" placeholder="Username" />
+                    <div className="mb-3">
+                        <input type="text" id="validationDefaultUsername" className="form-control" placeholder="Username" />
                     </div>
-                    <div class="mb-3">
-                        <input type="password" class="form-control" placeholder="Password" />
+                    <div className="mb-3">
+                        <input type="password"  id="validationDefaultPassword" className="form-control" placeholder="Password" />
                     </div>
                     <div>
                         <div style={{float: 'left', marginBottom: '20px'}} >
                         <select name="user-type" id="userType">
                             <option value="tipo0">User Type</option>
-                            <option value="Administrator">Administrator</option>
-                            <option value="User">User</option>
-                            <option value="Project Manager">Project Manager</option>
+                            <option value="admin">Administrator</option>
+                            <option value="user">User</option>
+                            <option value="gestor">Project Manager</option>
                         </select>
                     </div>
                     </div>
@@ -39,5 +40,29 @@ class CreateUser extends React.Component {
         )
     }
 } 
+
+
+function createUser (e) {
+    e.preventDefault();
+
+    let request =  {
+        nome: document.getElementById('validationDefaultName').value,
+        contacto: document.getElementById('validationDefaultContact').value,
+        username: document.getElementById('validationDefaultUsername').value,
+        password: document.getElementById('validationDefaultPassword').value,
+        tipo: document.getElementById('userType').value   
+    }
+
+    axios.post('http://localhost:4000/api/users', request)
+    .then( resp => {
+        alert("User successfully added!");
+        window.location.reload(false);
+    })
+    .catch( err => {
+        /* alert("Wrong Credentials!"); */
+    })
+}
+
+
 
 export default CreateUser;
